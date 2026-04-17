1c4b798b29bb096091f8b53176e789845e776788
saulfrancopaolon-tech <saulfrancopaolon@gmail.com>
2026-04-17 13:49:27 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css"
+
+export const metadata = {
+  title: 'BOTA-GEN | Admin',
+  description: 'Sistema de generación de códigos BOTA-NA',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="es">
-      <body>{children}</body>
+      <body className="bg-black">{children}</body>
     </html>
   )
 }
